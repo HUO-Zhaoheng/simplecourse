bf9484df4a04bd231c740c81e63e30e6c291210b
HenryHuo <413119123@qq.com>
2018-09-29 18:26:12 +0800

    Initial commit

---
+++ src/main/java/com/ddcs/simplecourse/SimplecourseApplication.java
@@ -0,0 +1,12 @@
+package com.ddcs.simplecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplecourseApplication.class, args);
+    }
+}
